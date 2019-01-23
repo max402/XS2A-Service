3ed802cbf94a989d06cdddb24407f5ed809ed067
Max <grischenko.maxim@gmail.com>
2019-01-23 12:12:36 +0100

    first commit

---
+++ src/main/java/de/casestudy/xs2a/Xs2aApplication.java
@@ -0,0 +1,14 @@
+package de.casestudy.xs2a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xs2aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xs2aApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/de/casestudy/xs2a/Xs2aApplicationTests.java
@@ -0,0 +1,17 @@
+package de.casestudy.xs2a;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Xs2aApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
